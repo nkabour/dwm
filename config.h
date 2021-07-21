52c9a2e4e403ff806c000d1f8404e775dfac35c1
nkabour <nadak.k@live.com>
2021-07-21 23:52:21 +0300

    removed browser hotkeys errors in making the dwm

--- config.h
+++ config.h
@@ -59,12 +59,12 @@ static const Layout layouts[] = {
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 static const char *termcmd[]  = { "alacritty", NULL };
-static const string browsercmd = "firefox"
+//static const string browsercmd = "firefox"
+//{ MODKEY,                       XK_b,      spawn,          {browsercmd} },
 
 static Key keys[] = {
 	/* modifier                     key        function        argument */
 	{ MODKEY,                       XK_r,      spawn,          {.v = dmenucmd } },
-	{ MODKEY,                       XK_b,      spawn,          {browsercmd} },
 	{ MODKEY,			            XK_Return, spawn,          {.v = termcmd } },
 	{ MODKEY|ShiftMask,				XK_b,      togglebar,      {0} },
 	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
